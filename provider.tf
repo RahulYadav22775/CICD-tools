7686e42ae25129cc02e350655eaa2f4343bef476
RahulYadav22775 <gopichand556677@gmail.com>
2024-12-08 14:57:20 +0530

    cicd-tools

---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+     bucket         = "gopi-81s-remote-state"
+    key            = "cide-tools"
+    region         = "us-east-1"
+    dynamodb_table = "gopi-81s-locking"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "zone_name" {
+  type        = string
+  default     = "gopi-81s.online"
+  description = "description"
+}
